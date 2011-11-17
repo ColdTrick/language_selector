d3f84f0922872d11f995872b0038a87e283f5b89
jdalsem <jdalsem@coldtrick.com>
2011-11-17 11:15:12 +0100

    fixed: moved header extension to pagesetup instead of init fixed: only reload translation if needed for logged out users

--- start.php
+++ start.php
@@ -6,30 +6,38 @@
 		global $CONFIG;
 		
 		if(!isloggedin()){
-			$new_lang_id = $_COOKIE['client_language'];
 			
-			if(!empty($new_lang_id)){
-				$CONFIG->language = $new_lang_id; 
+			if(!empty($_COOKIE['client_language'])){
+				// switched with language selector
+				$new_lang = $_COOKIE['client_language']; 
 			} else {
 				
 				$browserlang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 				
 				if(!empty($browserlang)){
+					// autodetect language
+					
 					if(get_plugin_setting("autodetect", "language_selector") == "yes"){
-						$CONFIG->language = $browserlang;
+						$new_lang = $browserlang;
 					}
 				}
 			}
 			
-			if(is_plugin_enabled("translation_editor")){
-				translation_editor_load_translations();
-			} else {
-				reload_all_translations();
+			if(!empty($new_lang) && ($new_lang !== $CONFIG->language)){
+				// set new language
+				$CONFIG->language = $new_lang;
+				
+				// language has been change; reload language keys
+				if(is_plugin_enabled("translation_editor")){
+					translation_editor_load_translations();
+				} else {
+					reload_all_translations();
+				}
 			}
 		}
 	}
 	
-	function language_selector_init(){
+	function language_selector_pagesetup(){
 		if(get_plugin_setting("show_in_header", "language_selector") == "yes"){
 			elgg_extend_view("page_elements/header_contents", "language_selector/default");
 		}
@@ -37,7 +45,7 @@
 	
 	// Default event handlers for plugin functionality
 	register_elgg_event_handler('plugins_boot', 'system', 'language_selector_plugins_boot');
-	register_elgg_event_handler('init', 'system', 'language_selector_init');
+	register_elgg_event_handler('pagesetup', 'system', 'language_selector_pagesetup');
 	
 	// actions
 	register_action('language_selector/change', false, dirname(__FILE__) . '/actions/change.php');
