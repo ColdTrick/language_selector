4facf3336b924136f453079a2e4b8433490dddc5
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-04-25 18:28:57 +0300

    feature(views): moves JS to AMD, moves flags to simplecache

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Features
 - handles translation preferences for logged in (user preferences) AND non logged in users (cookies)
 - incorporated autodetection of browser language (only for non logged in users)	
 - language selector display country codes or flags
+
+Notes
+-----
+
+* To add a language selector in a custom position, use ``elgg_view('language_selector/default')``
+* To add a custom language icon or replace an existing one, add an image file (svg, jpg, png, gif) in your plugin
+under `/views/default/language_selector/flags/$language_code.$ext`.
--- views/default/language_selector/default.php
+++ views/default/language_selector/default.php
@@ -1,71 +1,59 @@
-<?php 
+<?php
 
 $allowed = language_selector_get_allowed_translations();
 $current_lang_id = get_current_language();
 
-if (count($allowed) > 1) {
-	// show text or flags
-	$show_flags = false;
-	if (elgg_get_plugin_setting("show_images", "language_selector") != "no") {
-		$show_flags = true;
-	}
-	$result = "";
-	
-	foreach ($allowed as $lang_id) {
-		$lang_name = elgg_echo($lang_id);
-		$text = "";
-		$action = "";
-		
-		if (!empty($result)) {
-			$result .= " | ";
-		}
-
-		if ($current_lang_id != $lang_id) {
-			if (elgg_is_logged_in()) {
-				$action = elgg_add_action_tokens_to_url(elgg_get_site_url() . "action/language_selector/change?lang_id=" . $lang_id);
-			} else {
-				
-				$action = "javascript:setLanguage(\"" . $lang_id . "\");";
-			}				
-		} 
-		
-		if ($show_flags) {
-			
-			$flag_file = "mod/language_selector/_graphics/flags/" . $lang_id . ".gif";
-			
-			if (file_exists(elgg_get_root_path() . $flag_file)) {
-				$text = "<img src='" . elgg_get_site_url() . $flag_file . "' alt='" . $lang_name . "' title='" . $lang_name . "'>";
-			}
-		}
-		
-		if (empty($text)) {
-			$text = $lang_id;
-		}
-		
-		if (!empty($action)) {
-			$result .= "<a href='" . $action . "' title='" . $lang_name . "'>" . $text . "</a>";
-		} else {
-			$result .= $text;	
-		}				
-	}
-	
-	$result = "<div class='language_selector'>" . $result . "</div>";
-
-	if (!elgg_is_logged_in()) {
-		?>
-		<script type="text/javascript">
-			function setLanguage(lang_id) {
-				setCookie("client_language", lang_id, 30);
-				document.location.href = document.location.href;			
-			}
-			function setCookie(c_name,value,expiredays) {
-				var exdate = new Date();
-				exdate.setDate(exdate.getDate() + expiredays);
-				document.cookie = c_name + "=" + escape(value) + ";Path=/" + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
-			}
-		</script>
-		<?php
-	}
-
-	echo $result;
+if (count($allowed) <= 1) {
+	return;
 }
+
+elgg_require_js('language_selector/default');
+
+// show text or flags
+$show_flags = false;
+if (elgg_get_plugin_setting("show_images", "language_selector") != "no") {
+	$show_flags = true;
+}
+
+$options = [];
+
+foreach ($allowed as $lang_id) {
+	$lang_name = elgg_echo($lang_id);
+	$text = $lang_id;
+
+	if ($show_flags) {
+		$flag_view = false;
+		foreach (['svg', 'jpg', 'png', 'gif'] as $ext) {
+			$flag_icon = "language_selector/flags/$lang_id.$ext";
+			if (elgg_view_exists($flag_icon)) {
+				$flag_view = $flag_icon;
+				break;
+			}
+		}
+		if ($flag_view) {
+			$text = elgg_view('output/img', [
+				'src' => elgg_get_simplecache_url($flag_view),
+				'alt' => $lang_name,
+				'class' => 'language-selector-flag-icon',
+			]);
+		}
+	}
+
+	$class = ['language-selector-toggle'];
+	if ($current_lang_id == $lang_id) {
+		$class[] = 'elgg-state-selected';
+	}
+
+	$options[] = elgg_view('output/url', [
+		'text' => $text,
+		'href' => "action/language_selector/change?lang_id=$lang_id",
+		'class' => $class,
+		'title' => $lang_name,
+		'is_action' => true,
+		'data-language' => $lang_id,
+	]);
+}
+
+echo elgg_format_element('div', [
+	'class' => 'language_selector',
+		], implode(' | ', $options));
